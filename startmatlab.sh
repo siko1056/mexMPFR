96b6647aa9def9d78ee6e009b41742263167cbd1
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-06-14 15:16:50 +0200

    Remove unneeded script files.

--- startmatlab.sh
+++
@@ -1,4 +0,0 @@
-export LD_LIBRARY_PATH=MPFRDIR/mpfr/lib:MPFRDIR/gmp/lib
-export LIBRARY_PATH=MPFRDIR/mpfr/lib:MPFRDIR/gmp/lib
-export C_INCLUDE_PATH=MPFRDIR/mpfr/include:MPFRDIR/gmp/include
-matlab -r "addpath(genpath('MATLABMPFRDIR/')); cd MATLABMPFRDIR/mpfr; startup;"
