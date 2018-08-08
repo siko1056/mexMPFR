039fa5e213e7058fe5134210f46d351db8e108f0
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-08-08 15:46:00 +0200

    Tidy up test directory.

--- test/mex_mpfr_version_info.c
+++ test/mex_mpfr_version_info.c
@@ -1,19 +1,13 @@
-/* 
- * Kompilieren in Matlab mit
- *   mex -f mexopts.sh mex_mpfr.c
- *
- * Ausführen aus Matlab 
- *   mex_mpfr
+/**
+ * mex_mpfr_version_info  Display MPFR version information.
  */
 #include <stdio.h>
 #include "gmp.h"
 #include "mpfr.h"
 #include "mex.h"
 void mexFunction( int nlhs, mxArray *plhs[], 
-		  int nrhs, const mxArray*prhs[] )
-{
+		              int nrhs, const mxArray*prhs[] ) {
 	mexPrintf ("MPFR library: %-12s\nMPFR header: %s (based on %d.%d.%d)\n",
 		mpfr_get_version(), MPFR_VERSION_STRING, MPFR_VERSION_MAJOR,
 		MPFR_VERSION_MINOR, MPFR_VERSION_PATCHLEVEL);
-	return;
 }
